7b2b31fc115c30cdb39e06fae8a91676e506330b
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2024-05-28 12:03:53 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          chat.Connect
+          Sociopedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
