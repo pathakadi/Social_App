861bbb6ba062e6d290ac01170040ed92e2ce691a
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2024-05-28 09:41:38 +0530

    Update index.jsx

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          SocioLApp
+          chat.Connect
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to SocioLApp, Made By Aditya.
+          Welcome to chat.Connect, Made By Aditya.
         </Typography>
         <Form />
       </Box>
